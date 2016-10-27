58be2f6178937de16b9b9d7f4df4fb2fe844ed40
JJ Merelo <jjmerelo@gmail.com>
2016-10-27 19:10:50 +0200

    Añade ejemplo que faltaba

---
+++ ejemplos/solo.rb
@@ -0,0 +1,4 @@
+#Configuration file for chef-solo
+file_cache_path "/home/jmerelo/Tutoriales/nube-cep/infraestructura-como-servicio/"
+cookbook_path "/home/jmerelo/Tutoriales/nube-cep/infraestructura-como-servicio/chef/cookbooks"
+json_attribs "/home/jmerelo/Tutoriales/nube-cep/infraestructura-como-servicio/chef/node.json"
