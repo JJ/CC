653af1113c115df399106e32dd481d73379e8085
JJ Merelo <jjmerelo@gmail.com>
2017-09-28 07:29:29 +0200

    Añade script para crear infraestructura

---
+++ scripts/crea-asignatura.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use File::Copy;
+
+use v5.16;
+
+my $dir = shift || ".";
+my $previo = shift || "CC16-17";
+
+for my $d ( qw( objetivos proyecto sesiones ) ) {
+    eval {
+	mkdir("$dir/$d");
+    };
+
+    my $url = "http://raw.githubusercontent.com/JJ/$previo/master/$d/README.md";
+    `wget --backups=1  $url`;
+    move( 'README.md', "$dir/$d/" ) || die "Can't write file";
+}
+
+for my $f ( qw (.gitignore LICENSE Metodología_y_criterios_de_evaluación.md README.md)) {
+    `wget  --backups=1 https://raw.githubusercontent.com/JJ/$previo/master/$f`;
+}
