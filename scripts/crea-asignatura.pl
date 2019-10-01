9f7a2fccd863c9b73efaad798f10d965275f54ce
JJ Merelo <jjmerelo@gmail.com>
2019-10-01 12:34:20 +0200

    Actualiza

--- scripts/crea-asignatura.pl
+++ scripts/crea-asignatura.pl
@@ -8,9 +8,9 @@ use File::Copy;
 use v5.16;
 
 my $dir = shift || ".";
-my $previo = shift || "CC16-17";
+my $previo = shift || "CC-18-19";
 
-for my $d ( qw( objetivos proyecto sesiones ) ) {
+for my $d ( qw( objetivos proyectos sesiones ) ) {
     eval {
 	mkdir("$dir/$d");
     };
@@ -18,7 +18,11 @@ for my $d ( qw( objetivos proyecto sesiones ) ) {
     my $url = "http://raw.githubusercontent.com/JJ/$previo/master/$d/README.md";
     `wget --backups=1  $url`;
     move( 'README.md', "$dir/$d/" ) || die "Can't write file";
-}
+  }
+
+mkdir("$dir/src");
+
+
 
 for my $f ( qw (.gitignore LICENSE Metodología_y_criterios_de_evaluación.md README.md)) {
     `wget  --backups=1 https://raw.githubusercontent.com/JJ/$previo/master/$f`;
